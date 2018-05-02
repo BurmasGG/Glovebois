9d3f1577415388643d0ecb15e85d72c4aa6858c6
Unknown <pspeed16@student.aau.dk>
2018-05-02 12:33:48 +0200

    Added comments and cleaned up
    Both MusicGloveProcessing.pde file and MusicGloveArduino.ino file.

--- MusicGloveArduino/MusicGloveArduino.ino
+++ MusicGloveArduino/MusicGloveArduino.ino
@@ -1,14 +1,14 @@
+// Defining includes.
 #include "Wire.h"
-
 #include "I2Cdev.h"
-
 #include "MPU6050_6Axis_MotionApps20.h"
-//#include "MPU6050.h" // not necessary if using MotionApps include file
 
+// Initialising the MPU with the required address (0x69)
 MPU6050 mpu(0x69);
 
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+#define LED_PIN 13
 bool blinkState = false;
+
 bool volumeChange = false;
 const int indexPin = A2;
 const int middlePin = A1;
@@ -21,14 +21,6 @@ uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-// orientation/motion vars
-Quaternion q;           // [w, x, y, z]         quaternion container
-VectorInt16 aa;         // [x, y, z]            accel sensor measurements
-VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
-VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
-VectorFloat gravity;    // [x, y, z]            gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 ////////////////////////////////////////////
 ///////////////THRESHOLDS///////////////////
 ////////////////////////////////////////////
@@ -94,12 +86,6 @@ void setup() {
   Serial.println(F("Testing device connections..."));
   Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-  // wait for ready
-//  Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-//  while (Serial.available() && Serial.read()); // empty buffer
-//  while (!Serial.available());                 // wait for data
-//  while (Serial.available() && Serial.read()); // empty buffer again
-
   // load and configure the DMP
   Serial.println(F("Initializing DMP..."));
   devStatus = mpu.dmpInitialize();
@@ -126,9 +112,9 @@ void setup() {
     // 1 = initial memory load failed
     // 2 = DMP configuration updates failed
     // (if it's going to break, usually the code will be 1)
-//    Serial.print(F("DMP Initialization failed (code "));
-//    Serial.print(devStatus);
-//    Serial.println(F(")"));
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
   }
 
   // configure LED for output
